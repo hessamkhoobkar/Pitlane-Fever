d667f7395c61640e5e25bf9885e3b7cfc79207b9
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-04-13 22:31:15 +0330

    inti left

--- src/components/sidebars/RightSidebar.tsx
+++ src/components/sidebars/RightSidebar.tsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
+
+// tanstack query
+
+// fetch failed
+// fetch loadiong
+
+// standing list design
+// standing link
+
+// responsive design
+
 export default function RightSidebar() {
+  const [driverStandings, setDriverStandings] = useState([]);
+
+  useEffect(() => {
+    getStandings();
+  }, []);
+
+  async function getStandings() {
+    const standingsReq = await fetch(
+      "http://ergast.com/api/f1/current/driverStandings.json",
+    );
+    const standingsRes = await standingsReq.json();
+    setDriverStandings(
+      standingsRes.MRData.StandingsTable.StandingsLists[0].DriverStandings.slice(
+        0,
+        5,
+      ),
+    );
+  }
   return (
-    <div className="min-h-full grow">
-      <p>data here</p>
+    <div className="min-h-full w-full max-w-sm">
+      <img src="./imgs/pitlane-logo.png" alt="" />
+
+      {driverStandings.map((driver) => (
+        <div>
+          <span>
+            {driver.Driver.givenName} {driver.Driver.familyName}
+          </span>
+          <span>{driver.position}</span>
+          <span>{driver.points}</span>
+          <span>{driver.wins}</span>
+        </div>
+      ))}
     </div>
   );
 }
